9de902bcd7c4900fbb99342978aeb7df7db2aafd
Tony Hsu <s890081tonyhsu@gmail.com>
2013-09-07 23:15:32 +0800

    Put the plugin load-in loop.
    To load in the css and js file.

--- index.php
+++ index.php
@@ -1,17 +1,34 @@
-//This is a main page to control all the web status.
+<?php 
+	//This is a main page to control all the web status.
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="zh-TW">
 <head>
 	<meta charset="UTF-8">
-	<title></title>
+	<meta http-equiv="Content-Language" content="zh-TW">
+	<title>航向四處的氣球--大二誠班</title>
+	<?php
+		function Import_Plugin($Path,$ext){
+			$link_file=glob($Path."*".$ext);
+			return $link_file;
+		}
+		$js=Import_Plugin("./data/javascript/",".js");
+		foreach($js as $value){
+			echo "<script language=JavaScript src=\"".$value."\"></script>";
+		}
+		$css=Import_Plugin("./data/css/",".css");
+		foreach($css as $value){
+			echo "<link rel=stylesheet type=\"text/css\" href=\"".$value."\">";
+		}
+	?>
 </head>
 <body>
 	<?php
 		include './view/header.php';
 		include './view/left_col.php';
 		include './view/main.php';
-		include './view/right_col';
-		include ',/view/footer.php';
+		include './view/right_col.php';
+		include './view/footer.php';
 	?>
 </body>
 </html>
