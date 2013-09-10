60916fd58dc59ac546ec7c1430381135889b1d87
Tony Hsu <s890081tonyhsu@gmail.com>
2013-09-10 10:51:57 +0800

    Cover basic put in.
    Put in three picture and add a mixin scss function.

--- view/main.php
+++ view/main.php
@@ -1,2 +1,11 @@
-//This is the main page of the website.
-//Target: put pictures to make it change.
+<?php
+	//This is the main page of the website.
+	//Target: put pictures to make it change.
+?>
+
+<div id="mainpage">
+	<div id="cover1"><img src="./data/images/cover/1.jpg" alt=""></div>
+	<div id="cover2"><img src="./data/images/cover/2.jpg" alt=""></div>
+	<div id="cover3"><img src="./data/images/cover/3.jpg" alt=""></div>
+	<h3>The Title</h3>
+</div>
