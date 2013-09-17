db4b61b264b5fa0927f9d794798f7ef44cf5be96
s890081tonyhsu <s890081tonyhsu@gmail.com>
2013-09-17 10:45:34 +0800

    Remove sass cache

--- data/config.rb
+++ data/config.rb
@@ -6,6 +6,7 @@ css_dir = "css"
 sass_dir = "scss"
 images_dir = "images"
 javascripts_dir = "javascripts"
+cache = false
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
@@ -14,7 +15,7 @@ javascripts_dir = "javascripts"
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
